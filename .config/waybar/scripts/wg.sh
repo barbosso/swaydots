d606c4d1e5afb2fc342ae4d483a13a4322ac6f98
barbosso <kalilinuxxx662@gmail.com>
2022-10-10 02:58:10 +0300

    init

---
+++ .config/waybar/scripts/wg.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function status() {
+    if [ "$(nmcli device status | grep wg0 | grep --only-matching 'connected')" == 'connected' ];then
+        echo " UP"
+    else echo " DOWN"
+    fi
+}
+
+function connect() {
+    if [ "$(nmcli device status | grep wg0 | grep --only-matching 'connected')" == 'connected' ];then
+        nmcli connection down WireGuard 1>/dev/null 2>&1
+    else nmcli connection up WireGuard 1>/dev/null 2>&1
+    fi
+}
+
+case $1 in
+    help)
+        usage
+        ;;
+    status)
+        status
+        ;;
+    connect)
+        connect
+        ;;
+esac
+
+
+
+
+# if [ $(warp-cli status | grep update | cut -d ' ' -f 3) == 'Connected' ];then 
+#     warp-cli disconnect 1>/dev/null 2>&1
+# else warp-cli connect 1>/dev/null 2>&1
+# fi
+
+
+# echo $(warp-cli status | grep update | cut -d ' ' -f 3)
